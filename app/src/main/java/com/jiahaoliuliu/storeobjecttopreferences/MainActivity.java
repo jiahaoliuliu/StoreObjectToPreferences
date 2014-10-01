adcd8dd2e09a15cde200348d6a5263be1caf24ad
Jiahao Liu <jliu@astrata.eu>
2014-10-01 13:22:05 +0200

    Storing and retrieving correctly the string from the shared preferences

--- app/src/main/java/com/jiahaoliuliu/storeobjecttopreferences/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/storeobjecttopreferences/MainActivity.java
@@ -2,9 +2,12 @@ package com.jiahaoliuliu.storeobjecttopreferences;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.jiahaoliuliu.storeobjecttopreferences.Preferences.StringId;
 import com.jiahaoliuliu.storeobjecttopreferences.model.SimpleModel;
 
 
@@ -14,10 +17,16 @@ public class MainActivity extends ActionBarActivity {
 
     private Preferences preferences;
 
+    // Layouts
+    private TextView objectDataTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the text view
+        objectDataTextView = (TextView)findViewById(R.id.object_data_text_view);
 
         preferences = Preferences.SingletonHolder.INSTANCE;
         preferences.initialize(this);
@@ -29,6 +38,12 @@ public class MainActivity extends ActionBarActivity {
         String dataToBeSaved = gson.toJson(simpleModel, SimpleModel.class);
 
         Log.v(TAG, "Data to be saved in the preferences: \n" + dataToBeSaved);
-        preferences.setString(Preferences.StringId.SAMPLE_OBJECT_CONTENT, dataToBeSaved);
+        preferences.setString(StringId.SAMPLE_OBJECT_CONTENT, dataToBeSaved);
+
+        String dataRetrieved = preferences.getString(StringId.SAMPLE_OBJECT_CONTENT);
+        if (!TextUtils.isEmpty(dataRetrieved)) {
+            SimpleModel simpleModelRetrieved = gson.fromJson(dataRetrieved, SimpleModel.class);
+            objectDataTextView.setText(simpleModelRetrieved.toString());
+        }
     }
 }
